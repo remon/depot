26b34c1409f6354c36aecf74e4d9f55683447a79
Reymond <remmony@gmail.com>
2012-07-20 03:01:52 +0300

    Update config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
+require "paperclip"
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
-Paperclip.options[:command_path] = "C:\Program Files\ImageMagick-6.5.6-Q8"
+#Paperclip.options[:command_path] = "C:\Program Files\ImageMagick-6.5.6-Q8"
